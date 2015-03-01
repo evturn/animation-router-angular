23af9318af26c53420432ed5fbd2b87c5cb5c0dd
Evan Turner <evturn@gmail.com>
2015-02-28 22:20:57 -0500

    partial added for home

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var animateApp = angular.modeule('animateApp', ['ngRoute', 'ngAnimate']);
 
 animateApp.config(function($routeProvider) {
-
+	.when('/', {
+		templateUrl : 'partials/page-home.html',
+		controller  : 'mainController'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
 });
